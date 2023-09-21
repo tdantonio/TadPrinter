c2b2c9210713cae3067069512827ad0b56aa585f
Tomás D'Antonio <tdantonio@frba.utn.edu.ar>
2023-09-21 11:29:42 -0300

    serializacion de objetos genericos con atributos (faltan los hijos)

--- ruby/lib/document.rb
+++ ruby/lib/document.rb
@@ -1,16 +1,28 @@
 require './ruby/lib/tag.rb'
 
 class Document
-  attr_accessor :proc, :stack_tags
 
-  def initialize(&proc)
-    @proc = proc
+  def initialize(&bloque)
     @stack_tags = []
+    proc = bloque || {}
+    @tag_final = instance_eval(&proc)
   end
 
   def xml
-    tag_final = instance_eval(&proc)
-    tag_final.xml
+    @tag_final.xml
+  end
+
+  def serialize(objeto)
+    @tag_final = Tag.with_label(objeto.class.to_s.downcase)
+
+    # Se obtienen los atributos del objeto y se meten en un hash
+    getters = objeto.instance_variables.map{ |a| a.to_s.delete_prefix('@')}.select{|m| objeto.respond_to?m}
+    atributos = Hash.new
+    getters.each { |m| atributos[m] = objeto.send(m) }
+
+    # A partir del Hash se agregar los atributos al tag
+    with_attributes(@tag_final, atributos)
+
   end
 
   private def method_missing(name, *args, &proc)
@@ -39,6 +51,7 @@ class Document
   end
 end
 
+=begin
 documento = Document.new do
   alumno nombre: "Matias", legajo: "123456-7" do
     telefono { "1234567890" }
@@ -50,6 +63,38 @@ documento = Document.new do
 end
 
 puts documento.xml
+=end
+
+class Alumno
+  attr_reader :nombre, :legajo
+
+  def initialize(nombre, legajo, telefono)
+    @nombre = nombre
+    @legajo = legajo
+    @telefono = telefono
+  end
+end
+
+
+unAlumno = Alumno.new("Matias","123456-8", "1234567890")
+documento2 = Document.new
+documento2.serialize(unAlumno)
+puts documento2.xml
+
+
+=begin
+unEstado = Estado.new(3, 5, true)
+unAlumno = Alumno.new("Matias","123456-8", "1234567890", unEstado)
+
+variables = unAlumno.instance_variables
+atributo = variables.first
+mensaje = atributo.to_s.delete_prefix('@')
+puts unAlumno.respond_to?(mensaje)
+nombre = unAlumno.send(mensaje)
+puts nombre.class
+=end
+
+
 
 
 =begin
@@ -66,6 +111,7 @@ XML:
 Orden de ejecucion:
 documento = Document.new
 documento.tag =
+
 Tag.with_label('alumno').
   with_attribute('nombre', 'Matias').
   with_attribute('legajo', '123456-7').
